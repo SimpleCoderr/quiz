2382ed1238a772133499842c29a856fe598851c9
Danil Yakobchuk <danilyakobchuk00@mail.ru>
2023-07-06 14:54:10 +0700

    add ResultInfo component

--- src/entities/Result/ui/Result.tsx
+++ src/entities/Result/ui/Result.tsx
@@ -3,6 +3,7 @@ import { UndoOutlined } from "@ant-design/icons";
 import { getResultComment } from "../lib/getResultComment";
 import { MyButton } from "shared";
 import s from "./Result.module.scss";
+import ResultInfo from "./result-info/ResultInfo";
 
 type ResultProps = {
   countRightAnswer: number;
@@ -21,12 +22,7 @@ export const Result = ({
 
   return (
     <div className={s.resultBlock}>
-      <div>
-        Ваш резульат:{" "}
-        <span className={s.resultInfo}>
-          {countRightAnswer}/{totalCountAnswer}
-        </span>
-      </div>
+      <ResultInfo countRightAnswer={countRightAnswer} totalCountAnswer={totalCountAnswer}/>
       <div className={s.resultComment}>{resultComment}</div>
       <MyButton
         style={{ fontSize: "28px" }}
