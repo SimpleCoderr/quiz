3c77b59e8bd8c9ca6a866dd8f7535b0fbd259a48
Danil Yakobchuk <danilyakobchuk00@mail.ru>
2023-07-06 18:04:01 +0700

    small corrected code

--- src/app/models/initStates/quizInitState.ts
+++ src/app/models/initStates/quizInitState.ts
@@ -4,23 +4,23 @@ import { country } from "shared/const/countryAndCaptalObject";
 import { getRandomQuestionBlock } from "shared/lib/getRandomQuestionBlock";
 
 type QuizState = {
-    questions: questionBlock[];
-    page: number;
-    pageQuantity: number;
-    answers: (number | undefined)[];
-    resultsIsShow: boolean;
-  };
+  questions: questionBlock[];
+  page: number;
+  pageQuantity: number;
+  answers: (number | undefined)[];
+  resultsIsShow: boolean;
+};
 
-  const questions: questionBlock[] = []
+const questions: questionBlock[] = [];
 
-  for (let i = 0; i < 10; i++) {
-    questions.push(getRandomQuestionBlock(country, capitalsOfEurope))
-  }
-  
+for (let i = 0; i < 10; i++) {
+  questions.push(getRandomQuestionBlock(country, capitalsOfEurope));
+}
+
 export const quizInitialState: QuizState = {
-    questions,
-    page: 1,
-    pageQuantity: 5,
-    resultsIsShow: false,
-    answers: [],
-  };
+  questions,
+  page: 1,
+  pageQuantity: 5,
+  resultsIsShow: false,
+  answers: [],
+};
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,6 +1,6 @@
-export { getRandomQuestionBlock } from "./lib"
-export * from "./ui/MyButton/MyButton"
-export * from "./ui/MyCheckbox/MyCheckbox"
-export * from './types/index'
-export { useTypedSelector } from "./model/hooks/useTypedSelector"
-export { useAppDispatch } from "./model/hooks/useTypedDispatch"
+export { getRandomQuestionBlock } from "./lib";
+export * from "./ui/MyButton/MyButton";
+export * from "./ui/MyCheckbox/MyCheckbox";
+export * from "./types/index";
+export { useTypedSelector } from "./model/hooks/useTypedSelector";
+export { useAppDispatch } from "./model/hooks/useTypedDispatch";
