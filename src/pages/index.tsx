5cf7599185a4613ce61cdff66ab32822588dff9d
Danil Yakobchuk <danilyakobchuk00@mail.ru>
2023-07-06 15:13:08 +0700

    add header on MainPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
-import { Quiz } from 'widgets/Quiz'
-import s from './styles.module.scss'
+import { Quiz } from "widgets/Quiz";
+import s from "./styles.module.scss";
+import { ArrowDownOutlined } from "@ant-design/icons";
 
 const MainPage = () => {
-
   return (
     <div className={s.mainPage}>
-      <Quiz/>
+      <header className={s.headerPage}>
+        <div>Пройдите тест на знание столиц мира и узнайте свой результат!</div>
+        <ArrowDownOutlined />
+      </header>
+      <Quiz />
     </div>
-  )
-}
+  );
+};
 
-export default MainPage
+export default MainPage;
